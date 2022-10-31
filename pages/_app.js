9d9dbf43c1bb8cb12d6b747c2726116b09e517de
Cintia Ruiz <cintiaruizv@gmail.com>
2022-10-30 19:30:40 -0600

    Main post component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/app.scss";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/app.scss'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
