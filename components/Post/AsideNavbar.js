257be694d72348b38a45a534d81f732ce665f6fe
Cintia Ruiz <cintiaruizv@gmail.com>
2022-11-03 17:22:31 -0600

    Post reactions advance

--- components/Post/AsideNavbar.js
+++ components/Post/AsideNavbar.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import {React,useState,useEffect} from 'react'
+const axios = require('axios');
 
-export const AsideNavbar = ({ navs }) => {
+export const AsideNavbar = ({ navs, reactionsCount, id }) => {
+  
+  const [state, setState] = useState([]);
+  const [disabledB, setDisabledB] = useState(false);
+  function handleClick() {
+    //reactions.push("2");
+  const URL_API = 'http://localhost:8080/'
+    axios.patch(`${URL_API}posts/${id}/reactions`)
+    .then(function (response) {
+      const posts = response.data._id
+      setState([...state, "like"]);
+      setDisabledB(true)
+    console.log(state)  
+    })
+    .catch((error) => console.log(error))
+    
+  }
+
+  useEffect(() => {
+    setState(reactionsCount==null?[]:reactionsCount)
+  }, [reactionsCount]);
   return (
     <ul className='navbar-nav'>
       {
           navs.map((nav) => {
             return (
               <li key={nav.icon} className='nav-item text-center'>
-                <a className='nav-link nav-icons nav-icons--like '>
+                <a className='nav-link nav-icons nav-icons--like ' onClick={handleClick} disabled={disabledB}>
                   <span className='wrapper-icon texv2t-center'>
                     <img src={nav.icon} alt='' />
                   </span>
                 </a>
                 <span className='wrapper-icon--count'>
-                  {nav.amount}
+                  
+                  {
+                    nav.id=="heart"
+                  ?state.length
+                  :nav.amount}
                 </span>
               </li>
             )
--- components/PostDetail.js
+++ components/PostDetail.js
@@ -8,15 +8,16 @@ const axios = require('axios')
 
 export default function Detail({ post }) {
   const router = useRouter();
-
+  let reactions;
   const {id} = router.query
-  console.log(id);
+  console.log("cintia here");
   const URL_API = 'http://localhost:8080/'
   useEffect(() => {
     axios.get(`${URL_API}posts/${id}`)
       .then((response) => {
         const postData = response.data.data.post
-        console.log(postData);
+        reactions = postData.reactions;
+        console.log(reactions);
       })
       .catch((error) => console.log(error))
 
@@ -26,7 +27,7 @@ export default function Detail({ post }) {
       <div className="container post__detail">
         <div className="row mt-5">
           <div className="col-md-1 col-lg-1 d-none d-md-flex justify-content-center post-aside-left mt-5">
-            <AsideNavbar navs={navs} />
+            <AsideNavbar navs={navs} reactions={reactions} id= {id} />
           </div>
           <div className="col-md-11 col-lg-8">
             {/* <Post post={post} /> */}
